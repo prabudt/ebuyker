a00b108afa61dcd1fbf3edc983b85ade4cbe55a2
prabu.mca2015@gmail.com <prabu.mca2015@gmail.com>
2022-04-19 21:58:41 +0530

    canges

--- app/Http/Controllers/API/LoadController.php
+++ app/Http/Controllers/API/LoadController.php
@@ -132,8 +132,7 @@ class LoadController extends Controller
         if( JWTAuth::user()->user_type_id != 2) {            
             if(isset($data->booking) && !empty($data->booking)) {
                 if(JWTAuth::user()->id != $data->booking->user_id) {
-                    $data->booking = null;
-                    dd($data);
+                    $data->unsetRelation('booking');
                 }
             }
         }
