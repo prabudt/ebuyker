42afa483bdbd23dd55424eaa45bb862420f3c9bf
prabu.mca2015@gmail.com <104026037+prabu-thangavel@users.noreply.github.com>
2022-04-28 20:46:51 +0530

    canges

--- app/Http/Controllers/API/LoadController.php
+++ app/Http/Controllers/API/LoadController.php
@@ -234,7 +234,7 @@ class LoadController extends Controller
         });
         $data = $data->orderBy('id', 'desc')->get();
         
-        $result = collect();
+       /*  $result = collect();
         if( JWTAuth::user()->user_type_id != 2) {
             if(count($data) > 0) {
                 foreach ($data as $key => $value) {
@@ -251,7 +251,7 @@ class LoadController extends Controller
             }
         } else {
             $result = $data;
-        }
-        return $this->sendSuccess($result);
+        } */
+        return $this->sendSuccess($data);
     }
 }
